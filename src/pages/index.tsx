9bd4ba0cad1b55c1d59c8e14c1bea114c7cf6811
Andrés Court <andres@andrescourt.com>
2023-03-25 17:17:46 -0500

    font sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,7 +51,7 @@ const PostView = (props: PostWithUser) => {
           <span>{`@${author.username}`}</span>
           &nbsp;<span>{`· ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
